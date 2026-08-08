a1b311191d97a70e325e24690cb9e0926be2af56
PAMISETTY ABHIRAM <abhirampspk453@gmail.com>
2026-08-08 20:51:18 +0530

    Time: 0 ms (100%), Space: 44.4 MB (44.96%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.java
+++ 0061-rotate-list/0061-rotate-list.java
@@ -9,36 +9,34 @@
  * }
  */
 class Solution {
-    public ListNode lastNode(ListNode temp,int k){
-        int count=1;
-        while(temp!=null){
-            if(count==k) return temp;
-            count++;
+    public ListNode findNthNode(ListNode head,int k){
+        int len=1;
+        ListNode temp=head;
+        while(temp.next!=null){
+            if(len==k) return temp;
+            len++;
             temp=temp.next;
-
         }
+        // temp.next=head;
         return temp;
+        
     }
     public ListNode rotateRight(ListNode head, int k) {
-        ListNode temp=head;
+        if(head==null || head.next==null){
+            return head;
+        }
         int len=1;
-        if(head==null || head.next==null ||k==0){
-            return head;
-        }
+        ListNode temp=head;
         while(temp.next!=null){
-            temp=temp.next;
             len++;
+            temp=temp.next;
         }
-        k%=len;
-        if(k==len){
-            return head;
-        }
+        if(k%len==0) return head;
+        k=k%len;
         temp.next=head;
-        ListNode last=lastNode(head,len-k);
-        head=last.next;
-        last.next=null;
+        ListNode newHead=findNthNode(head,len-k);
+        head=newHead.next;
+        newHead.next=null;
         return head;
-
-        
     }
 }
